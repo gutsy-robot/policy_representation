c65d3cd47865a5c15e3e7a6cac66f5996d6e2ed3
Suhas <suhasraja1@gmail.com>
2020-08-12 20:04:14 -0500

    relative paths

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,7 @@
 from numpy import array
 import pickle as pk
 
-with open('baits_embedding_dict.pickle','rb') as fp:
+with open('agents/policy_representation/baits_embedding_dict.pickle','rb') as fp:
     bait_embedding_list = pk.load(fp)
-with open('shooters_embedding_dict.pickle','rb') as fp:
+with open('agents/policy_representation/shooters_embedding_dict.pickle','rb') as fp:
     shooter_embedding_list = pk.load(fp)
